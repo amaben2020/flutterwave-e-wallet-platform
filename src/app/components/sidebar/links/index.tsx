23c0bdfeda34e51e2fcaf0558bac07474cdea1dc
algomachine007 <amaben202phantom@gmail.com>
2023-08-13 13:16:22 +0100

    building the navbar icons

--- src/app/components/sidebar/links/index.tsx
+++ src/app/components/sidebar/links/index.tsx
@@ -1,5 +1,14 @@
-const Links = () => {
-  return <div>Links</div>;
+const LinkIcon = ({ categoryName, title, children }: any) => {
+  return (
+    <>
+      <h3 className="text-white ">{categoryName}</h3>
+
+      <div className="flex gap-5">
+        <div>{children}</div>
+        <p className="text-white">{title}</p>
+      </div>
+    </>
+  );
 };
 
-export default Links;
+export default LinkIcon;
