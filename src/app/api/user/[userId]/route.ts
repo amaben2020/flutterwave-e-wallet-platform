5d8b93331da505cd34f54455380feb1a420c2ae4
algomachine007 <amaben202phantom@gmail.com>
2023-08-23 17:44:36 +0100

    user update api created

--- src/app/api/user/[userId]/route.ts
+++ src/app/api/user/[userId]/route.ts
@@ -43,3 +43,50 @@ export const GET = async (req: Request, { params: { userId } }: any) => {
     await prisma.$disconnect();
   }
 };
+
+export const PUT = async (req: Request, { params: { userId } }: any) => {
+  try {
+    // await main();
+    console.log("CALLED ✅");
+    const body = await req.json();
+    const isExistingUser = await prisma.user.findFirst({
+      where: {
+        email: body.email,
+      },
+    });
+
+    if (isExistingUser) {
+      return NextResponse.json(
+        { message: "User already in DB conflict", user: isExistingUser },
+        { status: 409 },
+      );
+    } else {
+      const user = await prisma.user.upsert({
+        where: {
+          id: userId,
+        },
+        update: {
+          firstName: body.firstName,
+          lastName: body.lastName,
+          email: body.email,
+        },
+        create: {
+          ...body,
+        },
+      });
+
+      if (user) {
+        return NextResponse.json({ message: "Success", user }, { status: 200 });
+      }
+      return NextResponse.json(
+        { message: "User not found", user: {} },
+        { status: 404 },
+      );
+    }
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  } finally {
+    // disconnect when all is said and done
+    await prisma.$disconnect();
+  }
+};
